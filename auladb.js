591cae5b8264f56db813488ec805fb48bc5411e9
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2025-10-16 10:53:42 -0300

    Update auladb.js

--- auladb.js
+++ auladb.js
@@ -10,6 +10,17 @@ db("db_2ma").collection("Clientes").insertMany(
   // json formated database is pasted here
 )
 
-//Whenever you want to visualize data from a collection you can use this:
+// Whenever you want to visualize data from a collection you can use this:
 db("db_2ma").collection("Endereco_Clientes").find();
 
+// Whenever you want to find a specific row
+db("db_2ma").collection("Endereco_Clientes").find(
+    {codigo : 375}
+);
+
+// Whenever you want them to be projected in a specific way
+
+db("db_2ma").collection("Endereco_Clientes").find(
+    {codigo : 376},
+    {projection: {Nome : 1, Cidade : 1}}
+);
