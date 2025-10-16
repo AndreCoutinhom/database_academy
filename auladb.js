deddbf5546265378713a9eacb2def84faed50d9a
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2025-10-16 09:17:29 -0300

    Create auladb.js

---
+++ auladb.js
@@ -0,0 +1,9 @@
+// This is a command for creating a new collection
+
+db("database_name").createCollection("collection_name");
+
+// This is a command for inserting new data into a colletion
+
+db("db_2ma").collection("Clientes").insertMany(
+  // json formated database is pasted here
+)
