a4c334e69bc31830f727bab8df8b0e8de0797284
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2025-10-16 10:40:21 -0300

    Enhance auladb.js with comments for MongoDB queries
    Added comments for clarity on MongoDB operations.

--- auladb.js
+++ auladb.js
@@ -1,3 +1,5 @@
+// Javascript queries for MongoDB
+
 // This is a command for creating a new collection
 
 db("database_name").createCollection("collection_name");
@@ -7,3 +9,7 @@ db("database_name").createCollection("collection_name");
 db("db_2ma").collection("Clientes").insertMany(
   // json formated database is pasted here
 )
+
+//Whenever you want to visualize data from a collection you can use this:
+db("db_2ma").collection("Endereco_Clientes").find();
+
